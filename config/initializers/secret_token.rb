83d2d6ea991f0e2468536fff313e9ee44227c325
vagrant <vagrant@rails-dev-box>
2013-11-29 02:33:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virtualpedia::Application.config.secret_token = 'f6a4b12354987228da25143d91935a9001e2c7a579fcd05270991ead79f3fea802384a40d7a92c7303abbc69b5afe5459f93dae76f2a9ea3203eb56798fab66f'
