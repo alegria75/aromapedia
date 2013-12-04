6ecd97ff5112fdaefc7eeb18f56677d340f22280
vagrant <vagrant@rails-dev-box>
2013-12-04 17:33:10 +0000

    modified tagitems model

--- app/models/tag_items.rb
+++ app/models/tag_items.rb
@@ -1,4 +1,4 @@
-class TagItems < ActiveRecord::Base
+class TagItem < ActiveRecord::Base
   belongs_to :articles
   belongs_to :tags
   # attr_accessible :title, :body
--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,14 @@ ActiveRecord::Schema.define(:version => 20131204065322) do
   end
 
   create_table "tag_items", :force => true do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "tags", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
