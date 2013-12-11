99a87a0ee02297a28b256b06aece69ed33e27e94
vagrant <vagrant@rails-dev-box>
2013-12-11 08:23:56 +0000

    modified views

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,14 @@ class Article < ActiveRecord::Base
   has_many :tags, :through => :tag_items
 
   attr_accessible :body, :title
+
+  def self.alphasort
+  	order('lower(title)')
+  end
+
+  def self.groupbyletter
+  	alphasort.group_by do |article|
+  		article.title.first
+  	end
+  end
 end
